a9b4cc5802be001caa1860be5f3357952ef87ca9
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-23 18:32:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
 # MAIN FUNCTION
 def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
-    WIN.blit(draw_text, (WIDTH/2 - draw.text.get_width()/2, HEIGHT / 2 - draw_text.get_height()/2))
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT / 2 - draw_text.get_height()/2))
     pygame.display.update()
     pygame.time.delay(5000)
     
