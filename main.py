25b83530c4e766fd1ea1408d13ef7e1e9cb12908
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-23 15:48:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+import os
 
 # WINDOW
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1700, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Brawler")
 
@@ -9,10 +10,22 @@ pygame.display.set_caption("Space Brawler")
 # VARIABLE
 WHITE = (255, 255, 255)
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 110, 80
+
+#IMAGES
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets","spaceship_yellow.png"))
+YELLOW_SPACESHIP_IMAGE = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets","spaceship_red.png"))
+RED_SPACESHIP_IMAGE = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+BACKGROUND_IMAGE = pygame.image.load(os.path.join("Assets", "space.png"))
 
 # FUNCTION
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(BACKGROUND_IMAGE, (0, 0))
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    WIN.blit(RED_SPACESHIP_IMAGE, (300, 100))
+     
     pygame.display.update()
     
 def main():
